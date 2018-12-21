d425eefabae212e1530d855b172c5d48186d91d6
syrkles420 <syrklesburnz@gmail.com>
2018-12-21 15:57:19 +0000

    msm8998:disable custom audio policy and inherit oneplus extras

--- common.mk
+++ common.mk
@@ -502,5 +502,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
-# Inherit from oppo-common
-$(call inherit-product, device/oppo/common/common.mk)
+# Inherit from oneplus-extras
+$(call inherit-product, device/oneplus/extras/common.mk)
